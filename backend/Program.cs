a7ddd5d059db38198788232a94e8f3b41f5ab086
jmongrio <jmongrio@hotmail.com>
2024-06-24 15:58:26 -0600

    Add logging

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,6 @@
 using backend.DBContext;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +11,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+#region Logging
+Log.Logger =
+    new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.Console()
+        .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+
+builder.Host.UseSerilog();
+#endregion
 
 #region Database
 var connectionString = builder.Configuration.GetConnectionString("SQLServerConnection");
-builder.Services.AddDbContext<MovieCatalogContext>(options => 
+builder.Services.AddDbContext<MovieCatalogContext>(options =>
     options.UseSqlServer(connectionString));
 #endregion
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,4 +43,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting up");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ backend/Services/UserApp/GetAllUsers.cs
@@ -0,0 +1,70 @@
+﻿using backend.DBContext;
+using backend.Models.ENUM;
+using backend.Models.Response;
+using backend.Models.UserApp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Services.UserApp
+{
+    public class GetAllUsers
+    {
+        private readonly MovieCatalogContext _context;
+        public GetAllUsers(
+            MovieCatalogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> GetUsers()
+        {
+            GeneralResponse<List<UserModel>> response = new();
+            try
+            {
+                List<UserModel> users =
+                await _context.Users.Where(x =>
+                                                x.Status.Equals(ENTITY_STATUS.ACTIVE))
+                                    .Include(x => x.IdRolNavigation)
+                                    .Select(x => new UserModel
+                                    {
+                                        Id = x.Id,
+                                        FirstName = x.FirstName,
+                                        LastName = x.LastName,
+                                        Email = x.Email,
+                                        Rol = x.IdRolNavigation!.Name
+                                    })
+                                    .ToListAsync();
+
+                if (users is null)
+                {
+                    response = new()
+                    {
+                        StatusCode = 400,
+                        Message = "Users not found",
+                        Object = null
+                    };
+                    return new OkObjectResult(response);
+                }
+
+                response = new()
+                {
+                    StatusCode = 200,
+                    Message = "Users found",
+                    Object = users
+                };
+                return new OkObjectResult(response);
+            }
+            catch (Exception error)
+            {
+                response = new()
+                {
+                    StatusCode = 500,
+                    Message = error.Message,
+                    Object = null
+                };
+
+                return new ObjectResult(response);
+            }
+        }
+    }
+}
