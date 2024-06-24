f86a112c80f1a7b923b55b122c2a74f433bbbef0
jmongrio <jmongrio@hotmail.com>
2024-06-24 14:06:30 -0600

    Update nugets

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfadee7f5a987911f545d84f9e3067423e2890e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8daffbcab997db794d208420875512367323bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
