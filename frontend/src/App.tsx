2cc992ce9c36072245d36f650d4bebe7ac696458
jmongrio <jmongrio@hotmail.com>
2024-07-14 18:07:03 -0600

    Home and update routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './Pages/Access/Login'
 import { Signup } from './Pages/Access/Signup'
 import { Home } from './Pages/Home'
-import { Users } from './Pages/Users'
+import { Users } from './Pages/Dashboard/Users/Users'
 import { NotFound } from './Pages/HttpCodes/NotFound'
 import { Forbidden } from './Pages/HttpCodes/Forbidden'
 import { Unauthorized } from './Pages/HttpCodes/Unauthorized'
 import { LoadingComponent } from './Components/LoadingComponent'
 import { ProtectedRoute } from './Components/ProtectedRoute'
+import { CreateUser } from './Pages/Dashboard/Users/CreateUser'
+import { EditUser } from './Pages/Dashboard/Users/EditUser'
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<ProtectedRoute />}>
             <Route path="/dashboard/user" element={<Users />} />,
+            <Route path="/dashboard/user/new-user" element={<CreateUser />} />,
+            <Route path="/dashboard/user/edit-user/:id" element={<EditUser />} />,
             <Route path="/dashboard/l" element={<LoadingComponent />} />
         </Route>
 
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,10 +1,27 @@
+import { MovieHome } from "../Components/MovieHome";
 import Navbar from "../Components/NavbarComponent";
 
 export function Home() {
     return (
         <>
             <Navbar />
-            <h1>Home</h1>
+            <main className="container">
+                <div className="row">
+                    <MovieHome
+                        title={"Nobody"}
+                        year={2022}
+                        rating={7.9}
+                        image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaBUJOHmiD5YjxuvXd-zJfO3Rd7ke0EUl2dQ&s"}
+                        quality={"HD"} />
+                    <MovieHome
+                        title={"The Godfather"}
+                        year={1972}
+                        rating={8.0}
+                        image={"https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY281_CR4,0,190,281_.jpg"}
+                        quality={"HD"}
+                    />
+                </div>
+            </main>
         </>
     )
 }
